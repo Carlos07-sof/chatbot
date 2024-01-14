62d7270c4d3c99e4929a4f8a56bd50eae1d901f9
ED Robles <68660802+Carlos07-sof@users.noreply.github.com>
2024-01-14 16:40:12 -0600

    Update

--- chatbotAPI.py
+++ chatbotAPI.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-from chatbot import ChatBot 
-from datetime import datetime, timedelta
-import jwt  
+from chatbot import ChatBot
 
 class ChatbotAPI:
     def __init__(self):
         self.app = Flask(__name__)
         CORS(self.app)
-
         self.chat = ChatBot()
 
         self.app.route('/')(self.inicio)
@@ -21,16 +18,9 @@ class ChatbotAPI:
 
     def endpoint_chatbot(self):
         try:
-            data = request.get_json()
-            message = data.get('message')
-
-            res = self.chat.response(message.lower())
-
-            if not res:
-                return jsonify({'mensaje': 'Respuesta no encontrada'})
-            else:
-                return jsonify({'mensaje': res})
-
+            message = request.get_json().get('message')
+            response = self.chat.response(message.lower())
+            return jsonify({'mensaje': response}) if response else jsonify({'mensaje': 'Respuesta no encontrada'})
         except Exception as e:
             return jsonify({'mensaje': f'Error: {str(e)}'}), 500
 
@@ -40,13 +30,6 @@ class ChatbotAPI:
     def method_not_allowed(self, error):
         return make_response(jsonify({'error': 'Método no permitido'}), 405)
 
-    def run(self):
-        self.app.run(debug=True)
-
-# Crear una instancia de la aplicación fuera de __init__
-chatbot_api = ChatbotAPI()
-
-# Usar la instancia de la aplicación para el comando de Gunicorn
-app = chatbot_api.app
 if __name__ == '__main__':
-    app.run(debug=True)
+    chatbot_api = ChatbotAPI()
+    chatbot_api.app.run(debug=True)
