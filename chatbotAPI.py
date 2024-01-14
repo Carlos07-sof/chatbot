56afc76c67773a181e39324b1fb01b420089ba82
ED Robles <68660802+Carlos07-sof@users.noreply.github.com>
2024-01-14 16:49:59 -0600

    Update

--- chatbotAPI.py
+++ chatbotAPI.py
@@ -2,34 +2,30 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from chatbot import ChatBot
 
-class ChatbotAPI:
-    def __init__(self):
-        self.app = Flask(__name__)
-        CORS(self.app)
-        self.chat = ChatBot()
+app = Flask(__name__)
+CORS(app)
+chat = ChatBot()
 
-        self.app.route('/')(self.inicio)
-        self.app.route('/chatbot/', methods=['POST'])(self.endpoint_chatbot)
-        self.app.errorhandler(404)(self.not_found)
-        self.app.errorhandler(405)(self.method_not_allowed)
+@app.route('/')
+def inicio():
+    return jsonify({"mensaje": "Bienvenido al asistente UNACHAT"})
 
-    def inicio(self):
-        return jsonify({"mensaje": "Bienvenido al asistente UNACHAT"})
+@app.route('/chatbot/', methods=['POST'])
+def endpoint_chatbot():
+    try:
+        message = request.get_json().get('message')
+        response = chat.response(message.lower())
+        return jsonify({'mensaje': response}) if response else jsonify({'mensaje': 'Respuesta no encontrada'})
+    except Exception as e:
+        return jsonify({'mensaje': f'Error: {str(e)}'}), 500
 
-    def endpoint_chatbot(self):
-        try:
-            message = request.get_json().get('message')
-            response = self.chat.response(message.lower())
-            return jsonify({'mensaje': response}) if response else jsonify({'mensaje': 'Respuesta no encontrada'})
-        except Exception as e:
-            return jsonify({'mensaje': f'Error: {str(e)}'}), 500
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'No se encontró el recurso solicitado'}), 404)
 
-    def not_found(self, error):
-        return make_response(jsonify({'error': 'No se encontró el recurso solicitado'}), 404)
-
-    def method_not_allowed(self, error):
-        return make_response(jsonify({'error': 'Método no permitido'}), 405)
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return make_response(jsonify({'error': 'Método no permitido'}), 405)
 
 if __name__ == '__main__':
-    chatbot_api = ChatbotAPI()
-    chatbot_api.app.run(debug=True)
+    app.run(debug=True)
