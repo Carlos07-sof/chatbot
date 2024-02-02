24f79bf640558e1a0bd1e80592a07cdd485038bc
ED Robles <68660802+Carlos07-sof@users.noreply.github.com>
2024-02-02 11:50:25 -0600

    Update test

--- chatbotAPI.py
+++ chatbotAPI.py
@@ -1,57 +1,34 @@
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
-# from chatbot import ChatBot
 
-from datetime import datetime, timedelta
-import jwt  
+app = Flask(__name__)
+CORS(app)
 
-class ChatbotAPI:
-    def __init__(self):
-        self.app = Flask(__name__)
-        CORS(self.app)
+#GET
+@app.route('/', methods = ['GET'])
+def inicio():
+    return jsonify({"mensaje": "Bienvenido al asistente UNACHAT"}) 
+#POST
+@app.route('/chatbot/', methods =['POST'])
+def chatbot():
 
-        self.chat = ChatBot()
-
-        self.app.route('/')(self.inicio)
-        # self.app.route('/chatbot/', methods=['POST'])(self.endpoint_chatbot)
-        self.app.errorhandler(404)(self.not_found)
-        self.app.errorhandler(405)(self.method_not_allowed)
-        
-
-    def inicio(self):
+    try:
         client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        print(f"Dirección IP del cliente en el endpoint_chatbot: {client_ip}")
-        return jsonify({"mensaje": "Bienvenido al asistente UNACHAT"})
-        
+        return jsonify({"mensaje": client_ip}) 
 
-    # def endpoint_chatbot(self):
-    #     try:   
-            
-    #         data = request.get_json()
-    #         message = data.get('message')
+    except Exception as e:
+        return jsonify({'mensaje': f'Error: {str(e)}'})
 
-    #         res = self.chat.response(message.lower())
-    #         client_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    #         print(f"Dirección IP del cliente en el endpoint_chatbot: {client_ip}")
 
-    #         if not res:
-    #             return jsonify({'mensaje': 'Respuesta no encontrada'})
-    #         else:
-    #             return jsonify({'mensaje': res})
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'No se encontró el recurso solicitado'}), 404)
 
-    #     except Exception as e:
-    #         return jsonify({'mensaje': f'Error: {str(e)}'}), 500
 
-    def not_found(self, error):
-        return make_response(jsonify({'error': 'No se encontró el recurso solicitado'}), 404)
+@app.errorhandler(405)
+def not_found(error):
+    return make_response(jsonify({'error': 'Método no permitido'}), 405)
 
-    def method_not_allowed(self, error):
-        return make_response(jsonify({'error': 'Método no permitido'}), 405)
-
-    def run(self):
-        self.app.run(debug=True)
-    
 if __name__ == '__main__':
-    chatbot_api = ChatbotAPI()
-    chatbot_api.run(debug=False, host = '0.0.0.0')
+    app.run(debug=True)
 
