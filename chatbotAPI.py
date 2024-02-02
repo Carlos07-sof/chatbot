f08a2fa1386509945e0eb43bc4cb541db38b4c30
ED Robles <68660802+Carlos07-sof@users.noreply.github.com>
2024-02-02 13:32:37 -0600

    Update

--- chatbotAPI.py
+++ chatbotAPI.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 def obtener_hora_desde_ip(direccion_ip):
     try:
-        geolocalizador = Nominatim(user_agent="obtener_hora_desde_ip")
+        geolocalizador = Nominatim(user_agent="obtener_hora_desde_ip", timeout=10)  # Establece un tiempo de espera específico
         ubicacion = geolocalizador.geocode(direccion_ip, language='es')
+
         if ubicacion:
             tf = TimezoneFinder()
             zona_horaria = timezone(tf.timezone_at(lng=ubicacion.longitude, lat=ubicacion.latitude))
@@ -17,8 +18,12 @@ def obtener_hora_desde_ip(direccion_ip):
             return fecha_actual.strftime('%d/%m/%Y %H:%M:%S')
         else:
             return "No se pudo obtener la información de ubicación para la dirección IP proporcionada."
+
+    except requests.exceptions.Timeout:
+        return "El servicio de geolocalización tardó demasiado en responder. Inténtalo de nuevo más tarde."
     except Exception as e:
         return f"Error al obtener la información: {e}"
+
 
 @app.route('/obtener_hora', methods=['POST'])
 def obtener_hora():
